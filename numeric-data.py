1d0ab53f6da21d9368152143b85fa65fd66e134c
Osiris <ec2-user@ip-10-16-10-60.us-west-2.compute.internal>
2026-02-27 19:38:29 +0000

    tipos de datos numericos

---
+++ numeric-data.py
@@ -0,0 +1,68 @@
+#Se realizan operaciones basicas imprimiendo directamente a consola
+print(2+2)
+print(4-2)
+print(2*2)
+print(4/2)
+
+#Imprimimos un texto plano
+print("Python has three numeric types: int, float, and complex")
+
+#Creamor una variable llamada myValue y guardamos el valor de 1
+myValue = 1
+
+#Imprimimos el valor de la variable myValue
+print(myValue)
+
+#Para saber el tipo de una variable usamos type
+print(type(myValue))
+
+#Cuando usamos la funcion str() vamos a imprimirel valor de la variable como si fuera texto
+print(str(myValue) + "is of the data type" + str(type(myValue)))
+
+#Las variables en Py pueden cambiar los valores que les damos 
+myValue = 3.14
+
+#Imprimimos el valor de la variable myValue
+print(myValue)
+
+#Para saber el tipo de una variable usamos type
+print(type(myValue))
+
+#Cuando usamos la funcion str() vamos a imprimirel valor de la variable como si fuera texto
+print(str(myValue) + "is of the data type" + str(type(myValue)))
+
+#Las variables en Py pueden cambiar los valores que les damos 
+myValue = 5j
+
+#Imprimimos el valor de la variable myValue
+print(myValue)
+
+#Para saber el tipo de una variable usamos type
+print(type(myValue))
+
+#Cuando usamos la funcion str() vamos a imprimirel valor de la variable como si fuera texto
+print(str(myValue) + "is of the data type" + str(type(myValue)))
+
+#Las variables en Py pueden cambiar los valores que les damos 
+myValue = True
+
+#Imprimimos el valor de la variable myValue
+print(myValue)
+
+#Para saber el tipo de una variable usamos type
+print(type(myValue))
+
+#Cuando usamos la funcion str() vamos a imprimirel valor de la variable como si fuera texto
+print(str(myValue) + "is of the data type" + str(type(myValue)))
+
+#Las variables en Py pueden cambiar los valores que les damos 
+myValue = False
+
+#Imprimimos el valor de la variable myValue
+print(myValue)
+
+#Para saber el tipo de una variable usamos type
+print(type(myValue))
+
+#Cuando usamos la funcion str() vamos a imprimirel valor de la variable como si fuera texto
+print(str(myValue) + "is of the data type" + str(type(myValue)))
